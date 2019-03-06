92b15eba60ff12f0fc8886e0ae60038b2cc73fc4
Fredrik Fagerholm <fagerholm.fredrik@gmail.com>
2019-03-06 17:50:19 +0200

    Buil superpermutations from found route.

--- src/find_superpermutation.py
+++ src/find_superpermutation.py
@@ -3,6 +3,11 @@ import numpy as np
 from ortools.constraint_solver import pywrapcp
 from ortools.constraint_solver import routing_enums_pb2
 
+from metrics import permutation_distance
+
+def check_superpermutation(permutations, superpermutation):
+    return all(p in superpermutation for p in permutations)
+
 # Distance callback
 def create_distance_callback(dist_matrix):
     # Create a callback to calculate distances between cities.
@@ -13,8 +18,9 @@ def create_distance_callback(dist_matrix):
     return distance_callback
 
 def main():
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 3:
         file_path = sys.argv[1]
+        start = int(sys.argv[2])
     else:
         raise RuntimeError("Must provide the number of symbols as an argument")
 
@@ -24,33 +30,47 @@ def main():
     # Distance matrix
     dist_matrix = data['distance_matrix']
 
+    # number of "cities" in the traveling salesman problem
     tsp_size = len(permutations)
+    # should only find one route
     num_routes = 1
-    depot = 0
+    # permutation to start from
+    #start = 22
 
     # Create routing model
     if tsp_size > 0:
-        routing = pywrapcp.RoutingModel(tsp_size, num_routes, depot)
+        routing = pywrapcp.RoutingModel(tsp_size, num_routes, start)
         search_parameters = pywrapcp.RoutingModel.DefaultSearchParameters()
         # Create the distance callback.
         dist_callback = create_distance_callback(dist_matrix)
         routing.SetArcCostEvaluatorOfAllVehicles(dist_callback)
         # Solve the problem.
         assignment = routing.SolveWithParameters(search_parameters)
+        # if solution was found show it
         if assignment:
-            # Solution distance.
-            print("Total distance: " + str(assignment.ObjectiveValue()) + " miles\n")
             # Display the solution.
-            # Only one route here; otherwise iterate from 0 to routing.vehicles() - 1
+            # Only one route
             route_number = 0
             index = routing.Start(route_number) # Index of the variable for the starting node.
-            route = ''
-            while not routing.IsEnd(index):
-                # Convert variable indices to node indices in the displayed route.
-                route += str(permutations[routing.IndexToNode(index)]) + ' -> '
+            next_index = assignment.Value(routing.NextVar(index))
+
+            superpermutation = permutations[routing.IndexToNode(index)]
+
+            while not routing.IsEnd(next_index):
+                # Convert variable indices to node indices and build 
+                # the superpermutation.
+                # get node values (indices to permutations)
+                curr_node = routing.IndexToNode(index)
+                next_node = routing.IndexToNode(next_index)
+                # append the necessary suffix of the next permutation to the superpermutation
+                superpermutation += permutations[next_node][-dist_matrix[curr_node, next_node]:]
+                # get next indices
                 index = assignment.Value(routing.NextVar(index))
-            route += str(permutations[routing.IndexToNode(index)])
-            print("Route:\n\n" + route)
+                next_index = assignment.Value(routing.NextVar(index))
+
+            print('\nSuperpermutation:', superpermutation, sep='\n')
+            print('\nSuperpermutation length:', len(superpermutation), sep='\n')   
+            print('\nIs superpermutation:', check_superpermutation(permutations, superpermutation))
         else:
             print('No solution found.')
     else:

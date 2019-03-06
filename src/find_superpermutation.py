03d39138148af7856b23933bbd204b57de8fbf83
Fredrik Fagerholm <fagerholm.fredrik@gmail.com>
2019-03-06 18:46:58 +0200

    Clean up code.

--- src/find_superpermutation.py
+++ src/find_superpermutation.py
@@ -1,3 +1,6 @@
+"""
+
+"""
 import sys
 import numpy as np
 from ortools.constraint_solver import pywrapcp
@@ -17,25 +20,36 @@ def create_distance_callback(dist_matrix):
 
     return distance_callback
 
-def main():
-    if len(sys.argv) == 3:
-        file_path = sys.argv[1]
-        start = int(sys.argv[2])
-    else:
-        raise RuntimeError("Must provide the number of symbols as an argument")
 
-    data = np.load(file_path)
-    # Permutations
-    permutations = data['permutations']
-    # Distance matrix
-    dist_matrix = data['distance_matrix']
+def build_superpermutation(routing, assignment, permutations, dist_matrix):
+    # Display the solution.
+    # Only one route
+    route_number = 0
+    index = routing.Start(route_number) # Index of the variable for the starting node.
+    next_index = assignment.Value(routing.NextVar(index))
 
+    superpermutation = permutations[routing.IndexToNode(index)]
+
+    while not routing.IsEnd(next_index):
+        # Convert variable indices to node indices and build 
+        # the superpermutation.
+        # get node values (indices to permutations)
+        curr_node = routing.IndexToNode(index)
+        next_node = routing.IndexToNode(next_index)
+        # append the necessary suffix of the next permutation to the superpermutation
+        superpermutation += permutations[next_node][-dist_matrix[curr_node, next_node]:]
+        # get next indices
+        index = assignment.Value(routing.NextVar(index))
+        next_index = assignment.Value(routing.NextVar(index))
+    
+    return superpermutation
+
+    
+def run_tsp_search(permutations, dist_matrix, start):
     # number of "cities" in the traveling salesman problem
     tsp_size = len(permutations)
     # should only find one route
     num_routes = 1
-    # permutation to start from
-    #start = 22
 
     # Create routing model
     if tsp_size > 0:
@@ -48,33 +62,36 @@ def main():
         assignment = routing.SolveWithParameters(search_parameters)
         # if solution was found show it
         if assignment:
-            # Display the solution.
-            # Only one route
-            route_number = 0
-            index = routing.Start(route_number) # Index of the variable for the starting node.
-            next_index = assignment.Value(routing.NextVar(index))
-
-            superpermutation = permutations[routing.IndexToNode(index)]
-
-            while not routing.IsEnd(next_index):
-                # Convert variable indices to node indices and build 
-                # the superpermutation.
-                # get node values (indices to permutations)
-                curr_node = routing.IndexToNode(index)
-                next_node = routing.IndexToNode(next_index)
-                # append the necessary suffix of the next permutation to the superpermutation
-                superpermutation += permutations[next_node][-dist_matrix[curr_node, next_node]:]
-                # get next indices
-                index = assignment.Value(routing.NextVar(index))
-                next_index = assignment.Value(routing.NextVar(index))
+            superpermutation = build_superpermutation(routing, assignment, 
+                                                      permutations, dist_matrix)
 
             print('\nSuperpermutation:', superpermutation, sep='\n')
             print('\nSuperpermutation length:', len(superpermutation), sep='\n')   
             print('\nIs superpermutation:', check_superpermutation(permutations, superpermutation))
+
         else:
             print('No solution found.')
     else:
         print('Specify an instance greater than 0.')
+
+
+def main():
+    if len(sys.argv) == 3:
+        # file to read distance matrix from
+        file_path = sys.argv[1]
+        # permutation to start from
+        start = int(sys.argv[2])
+    else:
+        raise RuntimeError("Must provide the number of symbols as an argument")
+
+    data = np.load(file_path)
+    # Permutations
+    permutations = data['permutations']
+    # Distance matrix
+    dist_matrix = data['distance_matrix']
+
+    run_tsp_search(permutations, dist_matrix, start)
+    
 
 if __name__ == '__main__':
     main()
